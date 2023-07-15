349627bf4ad53d21e9be7c8716531651a0b405b5
SiNiquity <20628674+SiNiquity@users.noreply.github.com>
2023-07-14 20:33:55 -0700

    Initial commit

---
+++ config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    # Rust environment
+    fish_add_path $HOME/.cargo/bin/
+
+    # Fix issue preventing use of GPG signing
+    # https://github.com/fish-shell/fish-shell/issues/6643
+    set -gx GPG_TTY (tty)
+end
