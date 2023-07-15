e4d4bc7c6f53d5918842d98af0500563b5317a52
SiNiquity <20628674+SiNiquity@users.noreply.github.com>
2023-07-14 20:58:51 -0700

    Add variables for color and function for prompt

--- config.fish
+++ config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 
     # Rust environment
-    fish_add_path $HOME/.cargo/bin/
+    fish_add_path -g $HOME/.cargo/bin/
 
     # Fix issue preventing use of GPG signing
     # https://github.com/fish-shell/fish-shell/issues/6643
